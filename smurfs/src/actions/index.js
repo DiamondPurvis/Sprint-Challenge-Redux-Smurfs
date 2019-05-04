ae814b101524d4976fcd0b315eadf7ad353fe127
DiamondPurvis <purvisdiamond@gmail.com>
2019-05-04 15:07:50 -0400

    Get smurf added

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -13,3 +13,30 @@
    U - updateSmurf
    D - deleteSmurf
 */
+import axios from 'axios';
+
+const URL = "http://localhost:3333/smurfs"
+
+export const SMURF_POST = "SMURF_POST";
+export const SMURF_PUT = "SMURF_PUT";
+export const SMURF_DELETE = "SMURF_DELETE";
+
+export const SMURF_GET = "SMURF_GET";
+export const SMURF_ADDING = "SMURF_ADDING";
+export const SMURF_FETCHED = "SMURF_FETCHED"; 
+export const SMURF_ERROR = "SMURF_ERROR";
+
+export const getSmurfs = () =>  {
+  return dispatch => {
+    dispatch({ type: SMURF_GET});
+
+    axios
+    .get(URL)
+    .then(response => {
+      dispatch({type: SMURFS_FETCHED, payload: response.data});
+    })
+    .catch(error => {
+      dispatch({type: SMURF_ERROR, payload: error});
+    });
+  }
+};
